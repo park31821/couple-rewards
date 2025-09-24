f8ca0e1f1f3b697701c8ac0463aafba51075794a
park31821 <74476122+park31821@users.noreply.github.com>
2025-09-24 19:43:11 +1000

    feat: add initial user fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    token_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    couple = models.ForeignKey(
+        "Couple", on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+
+class Couple(AbstractUser):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
--- couple_rewards/settings.py
+++ couple_rewards/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "accounts",
 ]
 
 REST_FRAMEWORK = {
@@ -128,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
