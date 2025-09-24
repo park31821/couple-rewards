83cb654c6521b9895a25eea724660fe9926d56c8
park31821 <74476122+park31821@users.noreply.github.com>
2025-09-24 23:17:02 +1000

    feat: add User and Couple models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Couple
 
-# Register your models here.
+
+@admin.register(User)
+class CustomerUserAdmin(UserAdmin):
+    list_display = ["username", "email", "token_balance", "couple", "is_active"]
+    list_filter = ["is_active", "couple"]
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Couple Information", {"fields": ("token_balance", "couple")}),
+    )
+
+
+@admin.register(Couple)
+class CoupleAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at", "is_active"]
+    list_filter = ["is_active", "created_at"]
+    search_fields = ["name"]
