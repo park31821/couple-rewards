d81faeb97d6812b4dbb016ea39f57ebc7cfa019b
park31821 <74476122+park31821@users.noreply.github.com>
2025-09-26 22:56:54 +1000

    minor fix

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import TokenTransaction, Reward
-from accounts.models import User
 
 
 class TokenTransactionSerializer(serializers.ModelSerializer):
